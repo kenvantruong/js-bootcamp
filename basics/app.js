d6744598ead48a536fa7e06e9f34086674c83ec0
Ken Truong <kenvantruong@gmail.com>
2018-08-11 11:31:47 -0700

    challenge 3

--- basics/app.js
+++ basics/app.js
@@ -31,3 +31,31 @@ dogName += ' Truong'
 
 console.log(dogName);
 
+/* --------------------------
+=== - equality operator
+!== - no equal operator
+< - less than operator
+> - greater than operator
+<= - less than or equal to operator
+>= - greater than or equal to operator
+--------------------------*/ 
+
+let temp = 32
+let isFreezing = temp <= 32;
+
+console.log(isFreezing);
+
+
+// -----Challenge 3----------
+function price (i) {
+  if(i <= 7){
+    console.log('You are eligible for Child Discount');
+  } else if (i >= 65) {
+    console.log('You are eligible for Senior Discount');
+  } else {
+    console.log('You are in Standard Rate Category');
+  }
+}
+
+console.log(price(7));
+  
