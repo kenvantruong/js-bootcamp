4661b44b384b64bd675d06312009f931dc9d5d0d
Ken Truong <kenvantruong@gmail.com>
2018-08-11 08:27:44 -0700

    challenge1

--- basics/app.js
+++ basics/app.js
@@ -1,3 +1,26 @@
-let cardboard = 'box';
-
+//Hello World
 console.log('hello world');
+
+//Strings
+let city = 'San Jose '
+let county = 'Silicon Valley '
+let state = 'California '
+let country = 'United States '
+
+console.log(city + county + state + country);
+
+//Numbers
+let age = 26;
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+//Challenge Area
+let student1 = 1800
+let maxScore = 2000
+
+let testScore = student1 / maxScore
+
+console.log(Math.ceil(testScore) +'%');
+
+
